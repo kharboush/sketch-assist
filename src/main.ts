7ee1a10d70f004717165019e5abf75f39d16333a
Anton Kharboush <anton@kharboush.com>
2020-08-17 12:54:01 +0300

    prepping for deployment

--- src/main.ts
+++ src/main.ts
@@ -9,12 +9,12 @@ async function bootstrap() {
   app.enableCors();
   app.use(helmet());
   app.useGlobalPipes(new ValidationPipe());
-  // app.use(
-  //   rateLimit({
-  //     windowMs: 5 * 60 * 1000, // 15 minutes
-  //     max: 100, // limit each IP to 100 requests per windowMs
-  //   }),
-  // );
-  await app.listen(5000);
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 500, // limit each IP to 500 requests per windowMs
+    }),
+  );
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
