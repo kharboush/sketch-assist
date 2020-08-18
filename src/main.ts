7c8bd17f20b0bfe74f3834f658f92db0bdc7742b
Anton Kharboush <anton@kharboush.com>
2020-08-18 11:36:43 +0300

    another reorder

--- src/main.ts
+++ src/main.ts
@@ -11,8 +11,8 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.use(
     rateLimit({
-      windowMs: 15 * 60 * 1000,
-      max: 500,
+      windowMs: 5 * 60 * 1000,
+      max: 100,
     }),
   );
   await app.listen(process.env.PORT || 5000);
