34bdb8808d92f72ec36fb812f8ddd5ae7560fe02
anton@kharboush.com <anton@kharboush.com>
2020-08-19 17:48:10 +0300

    moving things around

--- src/assistant/statics.ts
+++ src/assistant/statics.ts
@@ -1,3 +1,23 @@
+export const info = {
+  title: `Sketch Assistant Generator API`,
+  ver: `0.4.1`,
+  desc: `
+## Why this exists?
+Creating a Sketch Assistant, and defining the rules that you want in it requires knowledge of JavaScript and NPM.
+
+This API eliminates the need to create assistant packages manually and exposes multiple configurable rules.
+
+A front-end will be coming soon, as well as a larger selection of rules.
+
+## Guide to this API:
+1. See all rules available with a GET request.
+2. Request your package using POST, receive an ID.
+3. Use the ID as a param to a GET request and receive your download.
+
+by @antonkharboush
+`,
+};
+
 const config = {
   active: false,
 };
@@ -253,24 +273,4 @@ export const pkgTemplate = {
     'webpack-cli': '3.3.12',
   },
   dependencies: {},
-};
-
-export const info = {
-  title: `Sketch Assistant Generator API`,
-  ver: `0.4.1`,
-  desc: `
-## Why this exists?
-Creating a Sketch Assistant, and defining the rules that you want in it requires knowledge of JavaScript and NPM.
-
-This API eliminates the need to create assistant packages manually and exposes multiple configurable rules.
-
-A front-end will be coming soon, as well as a larger selection of rules.
-
-## Guide to this API:
-1. See all rules available with a GET request.
-2. Request your package using POST, receive an ID.
-3. Use the ID as a param to a GET request and receive your download.
-
-by @antonkharboush
-`,
 };
